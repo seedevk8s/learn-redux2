20f26b7eb9053ff72debf0345b5a0e23c757ebaa
unknown <D191375@HYUNDAI-GLOVIS.COM>
2019-08-27 17:25:28 +0900

    App.js 부분을 함수형에서 클래스형으로 변경함

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 import Palette from './components/Palette';
 import Counter from "./components/Counter";
 import WaitingList from "./components/WaitingItem";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette selected="red" />
-      <Counter value={0} color="red" />
-      <WaitingList/>
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <Palette selected="red" />
+                <Counter value={0} color="red" />
+                <WaitingList/>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
