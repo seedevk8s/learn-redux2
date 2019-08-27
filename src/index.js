8848fdd21286b6e9da05b6b84d7f86a8394f77c6
unknown <D191375@HYUNDAI-GLOVIS.COM>
2019-08-27 17:18:23 +0900

    스토어까지 잘 만들어짐

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// (1) createStore와 루트 리듀서 불러오기
+import {createStore} from "redux";
+import rootReducer from './store/modules';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// (2) 스토어를 만들고 현재 값 확인해 보기
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/store/modules/counter.js
@@ -0,0 +1,33 @@
+// 액션 타입 정의
+const CHANGE_COLOR = 'counter/CHANGE_COLOR';
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// 액션 생성함수 정의
+export const changeColor = color => ({  type: CHANGE_COLOR, color});
+export const increment = () => ( { type: INCREMENT } );
+export const decrement = () => ({ type: DECREMENT });
+
+// 초기 상태 정의
+const initialState = {
+  color: 'red',
+  number: 0,
+};
+
+// 리듀서 작성
+export default function  counter(state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_COLOR:
+            return {
+              ...state,
+              color: action.color,
+            };
+        case DECREMENT:
+            return {
+              ...state,
+              number: state.number - 1,
+            };
+        default:
+            return state;
+    }
+}
