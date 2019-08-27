37e0b8866c3feac0377aa2308303b4a14d872c5f
unknown <D191375@HYUNDAI-GLOVIS.COM>
2019-08-27 16:16:11 +0900

    팔레트 아래 숫자와 버튼 표시추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Palette from './components/Palette';
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <Palette selected="red" />
+      <Counter value={0} color="red" />
     </div>
   );
 }
